49847c58d96fad9c5b4fa944dd8646a7330dcd54
skullzmedia <skullzmedia@gmail.com>
2025-04-10 22:51:32 +0530

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from 'astro-sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.phoenixcasino.in/', // Replace with your actual domain
+  integrations: [
+    sitemap(),
+  ],
+});
