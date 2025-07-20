78632ef6b06a2429dfb07e96d76815960877c6bd
skullzmedia <skullzmedia@gmail.com>
2025-07-20 02:17:27 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from 'astro-sitemap';
 
 export default defineConfig({
-  site: 'https://www.phoenixcasino.in', // Make sure no trailing slash here
-  integrations: [
-    sitemap({
-      sitemap: '/sitemap.xml', // Force output to sitemap.xml
-    }),
-  ],
+  site: 'https://www.phoenixcasino.in/',
+  integrations: [sitemap()],
 });
