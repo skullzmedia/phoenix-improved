04b7d190960d440e88980f2d6dbedae0fe9a418a
skullzmedia <skullzmedia@gmail.com>
2025-07-29 13:08:51 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,5 @@ import sitemap from 'astro-sitemap';
 
 export default defineConfig({
   site: 'https://www.phoenixcasino.in',
-  integrations: [
-    sitemap({
-      filter: (page) => !page.pathname.includes('undefined'),
-      serialize: ({ url }) => ({
-        url: url.pathname,
-        lastmod: new Date().toISOString(),
-      }),
-    }),
-  ],
+  integrations: [sitemap()],
 });
